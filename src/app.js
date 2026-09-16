6a6c135e1520bf1093c1ebdedac268f296bf11f8
Harsh Jaim <harshjn782@gmail.com>
2026-09-16 15:29:03 +0530

    Adding middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const authRouter = require('../src/routes/auth.routes')
 const cookieParser = require('cookie-parser')
-
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
+// Routes required
+const authRouter = require('../src/routes/auth.routes')
+const accountRouter = require('../src/routes/account.routes')
+
+
+// Use Routes
 app.use('/api/auth', authRouter)
+app.use('/api/accounts', accountRouter)
 
 module.exports = app
